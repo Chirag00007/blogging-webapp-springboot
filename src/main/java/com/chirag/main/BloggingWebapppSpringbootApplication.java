d011f0e123863b3f839ef632c824a9b60317e2cc
CHIRAG VOHRA <chiragvohra39470@gmail.com>
2023-05-07 16:09:03 +0530

    setup

---
+++ src/main/java/com/chirag/main/BloggingWebapppSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.chirag.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingWebapppSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloggingWebapppSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chirag/main/BloggingWebapppSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chirag.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingWebapppSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
