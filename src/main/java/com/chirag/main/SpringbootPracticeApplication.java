131cf73e9c5464071c3631400b2967d7d6e05918
Chirag00007 <chiragvohra39470@gmail.com>
2023-07-07 18:28:08 +0530

    changes in spa

--- src/main/java/com/chirag/main/SpringbootPracticeApplication.java
+++ src/main/java/com/chirag/main/SpringbootPracticeApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.ApplicationContext;
 public class SpringbootPracticeApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = SpringApplication.run(SpringbootPracticeApplication.class, args);
+     SpringApplication.run(SpringbootPracticeApplication.class, args);
 
 	}
 
